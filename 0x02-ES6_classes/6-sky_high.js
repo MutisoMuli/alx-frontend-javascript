a45d0f8c8c7ebaf733fc84f29f143183b4b5bee4
MutisoMuli <mulimutiso525@gmail.com>
2024-05-30 23:23:24 +0300

    Done

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -15,6 +15,6 @@ export default class SkyHighBuilding extends Building {
   }
 
   evacuationWarningMessage() {
-    return `Evvacuate slowly the ${this.floors} floors`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
